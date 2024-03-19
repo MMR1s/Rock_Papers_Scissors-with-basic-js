c40a47956040459540e5d9c2a3e03af3ab89b1bd
MMR1s <135247082+MMR1s@users.noreply.github.com>
2024-03-19 05:41:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const choises = ["rock ✊", "paper 🤚", "scissors ✌️"];
+const playerDisplay = document.querySelector("#playerDisplay");
+const computerDisplay = document.querySelector("#computerDisplay");
+const resultDisplay = document.querySelector("#resultDisplay");
+let playerCounter = 0;
+let computerCounter = 0;
+
+function playGame(playerChoice) {
+  const computerChoice = choises[Math.floor(Math.random() * choises.length)];
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "It's a tie";
+  } else {
+    switch (playerChoice) {
+      case "rock ✊":
+        result = computerChoice === "scissors" ? "you won" : "you lost";
+        break;
+      case "paper 🤚":
+        result = computerChoice === "rock" ? "you won" : "you lost";
+        break;
+      case "scissors ✌️":
+        result = computerChoice === "paper" ? "you won" : "you lost";
+        break;
+    }
+  }
+
+  playerDisplay.textContent = `Player : ${playerChoice} `;
+  computerDisplay.textContent = `Computer : ${computerChoice}`;
+  resultDisplay.textContent = result;
+
+  resultDisplay.classList.remove("greenText", "redText");
+
+  switch (result) {
+    case "you won":
+      resultDisplay.classList.add("greenText");
+      playerCounter++;
+      document.getElementById("playerCounter").textContent = playerCounter;
+      break;
+    case "you lost":
+      resultDisplay.classList.add("redText");
+      computerCounter++;
+      document.getElementById("computerCounter").textContent = computerCounter;
+      break;
+  }
+}
